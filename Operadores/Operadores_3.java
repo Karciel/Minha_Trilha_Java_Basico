44de0d8249599229752d49b061a1ac8d6b381568
Gabriel V. Silva <karciel@outlook.com>
2023-05-06 18:48:13 -0300

    Operadores_3

---
+++ Operadores/Operadores_3.java
@@ -0,0 +1,28 @@
+public class Operadores_3 {
+    public static void main(String [] args) {
+    
+    int numero = 5;
+    numero = - numero;
+
+    System.out.println (numero * -1);
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
