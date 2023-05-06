2fee7d4c4d6293e7b5209c29151024dfa5e5c6c6
Gabriel V. Silva <karciel@outlook.com>
2023-05-06 16:39:12 -0300

    Operadores_parte 1

---
+++ Operadores/operadores_1.java
@@ -0,0 +1,30 @@
+public class operadores_1 {
+
+    public static void main (String[] args) {
+        String nomeCompleto = "LINGUAGEM" + (" ") + "JAVA";
+        
+        
+        System.out.print(nomeCompleto);
+
+
+// Operador "+" para fazer a concatenação de STRINGS
+
+/* Pederia fazer desta forma também:
+
+String firstName = "John";
+String lastName = "Doe";
+String fullName = firstName.concat(" ").concat(lastName);
+
+System.out.println(fullName); // Saída: John Doe */
+
+
+
+
+
+
+    }
+
+
+
+
+}
