f9d4f42b6e888282c87de9f1cca87d19319b09b2
Gabriel V. Silva <karciel@outlook.com>
2023-05-06 21:15:25 -0300

    Operadores_4_all

---
+++ Operadores/Operadores_4.java
@@ -0,0 +1,33 @@
+public class Operadores_4{
+    public static void main(String[] args) {
+        int numero = 5;
+
+    //(++ incrementa +1)
+
+    //System.out.println(numero ++); 
+    System.out.println(++ numero);
+    /*Ao colocar o operador ++ depois da variável, o resultado da incrementação só aparecerá na próxima vez que a variável for chamada (numero ++)
+    Contudo, se colocarmos o operador ++ antes da variável, o resultado da incrementação aparecerá imediatamente! (++ numero)*/
+    System.out.println(numero); 
+
+/*  
+
+=: atribui um valor a uma variável
++=: adiciona um valor a uma variável
+-=: subtrai um valor de uma variável
+*=: multiplica uma variável por um valor
+/=: divide uma variável por um valor
+%=: atribui o resto da divisão de uma variável por um valor
+++: aumenta o valor de uma variável em 1
+--: diminui o valor de uma variável em 1
+
+Esses operadores são usados para modificar o valor de uma variável de maneira mais concisa */
+
+
+
+
+            
+        }
+            
+}
+
