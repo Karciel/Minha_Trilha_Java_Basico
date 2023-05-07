fd83be05665a5f197f61e18c2dc49e854748a6c3
Gabriel V. Silva <karciel@outlook.com>
2023-05-07 19:09:38 -0300

    Operadores lógicos

---
+++ Operadores/Operadores_7.java
@@ -0,0 +1,31 @@
+public class Operadores_7 {
+    public static void main(String[] args) {
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+
+
+        if (condicao1 ^ condicao2) {
+
+
+
+      System.out.println("As duas condições são verdadeiras");
+      
+        }
+
+        System.out.println("Fim");
+            
+            
+/*
+
+&& : E lógico. Retorna verdadeiro se ambos os operandos forem verdadeiros.
+|| : OU lógico. Retorna verdadeiro se pelo menos um dos operandos for verdadeiro.
+! : NÃO lógico. Inverte o valor lógico do operando.
+^ : OU exclusivo. Retorna verdadeiro se exatamente um dos operandos for verdadeiro.
+
+*/
+        }
+
+
+
+
+    }
