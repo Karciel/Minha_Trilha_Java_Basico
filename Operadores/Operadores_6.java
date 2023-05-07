b69560724f6881ea6f71ba1e7e075ffdbfa9ae85
Gabriel V. Silva <karciel@outlook.com>
2023-05-07 14:19:06 -0300

    Operadores relacionais

---
+++ Operadores/Operadores_6.java
@@ -0,0 +1,32 @@
+public class Operadores_6 {
+    public static void main(String[] args) {
+        int numeroUm = 4;
+        int numeroDois = 6;
+
+        System.out.println("Eles são iguais: " + (numeroUm == numeroDois));
+
+        System.out.println("São diferentes: " + (numeroUm != numeroDois));
+
+        System.out.println("numeroUm é maior que numeroDois? - " + (numeroUm > numeroDois));
+
+        System.out.println("numeroUm é igual ou maior que numeroDois - " + (numeroUm >= numeroDois));
+
+        System.out.println("numeroUm é menor que numeroDois? - " + (numeroUm < numeroDois));
+
+        System.out.println("numeroUm é igual ou menor que numeroDois? - " + (numeroUm <= numeroDois));
+
+
+// Todos os operadores RELACIONAIS: 
+
+/* 
+
+==   (igual a)
+!=   (diferente de)
+>    (maior que)
+>=   (maior ou igual a)
+<    (menor que)
+<=   (menor ou igual a)
+
+*/
+    }
+}
