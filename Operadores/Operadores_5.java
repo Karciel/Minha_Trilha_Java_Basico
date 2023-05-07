db16f35f8bc90db6c008272e4910ed054e416e72
Gabriel V. Silva <karciel@outlook.com>
2023-05-07 11:19:48 -0300

    Operador ternário

--- Operadores/Operadores_5.java
+++ Operadores/Operadores_5.java
@@ -2,17 +2,24 @@ public class Operadores_5{
     public static void main(String[] args){
         int a, b;
 
-        a = 5;
-        b = 6;
+        a = 4;
+        b = 4;
 
-        String resultado = "";
+        //Operador ternário - O mesmo pode substituir if e else, deixando o código mais sucinto. Ex:
+        //String resultado = a==b ? "Verdadeira": "Falso";
 
-        if (a==b)
+        //O operador ternário não se limita a strings, podendo ser usado para valores do tipo inteiro, por exemplo.
+        int resultado = a==b ? 1 : 2;
+
+        System.out.println("O resultado é: " + resultado);
+    
+
+        /* if (a==b)
             resultado = "verdadeiro";
         else 
             resultado = "falso";
 
-        System.out.println("O resultado é:" + resultado);
+        System.out.println("O resultado é: " + resultado); */
 
     }
     
