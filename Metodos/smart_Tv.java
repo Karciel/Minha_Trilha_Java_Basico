e9394c2c1bbd7484db37994d9efb2daf1f8081a3
Gabriel V. Silva <karciel@outlook.com>
2023-05-08 21:19:37 -0300

    Comentario sobre a definicao de "METODO" em JAVA

--- Metodos/smart_Tv.java
+++ Metodos/smart_Tv.java
@@ -21,9 +21,9 @@ public class smart_Tv {
         ligada=false;
     }
     
-
-
-
-    
 }   
 
+/*Em resumo, um método em Java é um bloco de código ou coleção de instruções que executa uma tarefa específica.
+Ele fornece a reutilização do código, pois escrevemos um método uma vez e o usamos várias vezes.
+Não precisamos escrever código repetidamente. 
+Ele também fornece a fácil modificação e legibilidade do código */
